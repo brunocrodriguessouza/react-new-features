5c43ae2286d4f4dde6454fa57fff8c1b4dca8126
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-06-27 13:53:14 -0300

    [01] The useState Hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = (props) => {
+    const [count, setCount] = useState(props.count)
+
+    const increment = () => {
+        setCount(count +1 )
+    }
+
+    const decrement = () => {
+        setCount(count -1)
+    }
+
+    const reset = () => {
+        setCount(props.count)
+    }
+
+    return(
+        <div>
+            <p>The current count is {count}</p>
+            <button onClick={() => decrement()}>-1</button>
+            <button onClick={() => reset()}>Reset</button>
+            <button onClick={() => increment()}>+1</button>
+        </div>
+    )
+}
+
+App.defaultProps = {
+    count: 0
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
