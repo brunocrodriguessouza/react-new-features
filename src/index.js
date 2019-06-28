2f5a3c47fa5a7523d1c86a2d08aef496c7904059
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-06-27 23:45:13 -0300

    [02] useState vs setState functional component

--- src/index.js
+++ src/index.js
@@ -3,26 +3,40 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 const App = (props) => {
-    const [count, setCount] = useState(props.count)
+    // const [count, setCount] = useState(props.count)
+    // const [text, setText] = useState('')
+    const [state, setState] = useState({
+        count: props.count,
+        text: ''
+    })
 
     const increment = () => {
-        setCount(count +1 )
+        // setCount(count + 1)
+        setState({ ...state, count: state.count +1 })
     }
 
     const decrement = () => {
-        setCount(count -1)
+        setState({ ...state, count: state.count -1 })
     }
 
     const reset = () => {
-        setCount(props.count)
+        setState({ ...state, count: props.count })
+    }
+
+    const changeInputValue = (e) => {
+        setState({ 
+            text: e.target.value, 
+            count: state.count 
+        })
     }
 
     return(
         <div>
-            <p>The current count is {count}</p>
+            <p>The current {state.text || 'count'} is {state.count}</p>
             <button onClick={decrement}>-1</button>
             <button onClick={reset}>Reset</button>
             <button onClick={increment}>+1</button>
+            <input value={state.text} onChange={(e) => changeInputValue(e)}/>
         </div>
     )
 }
